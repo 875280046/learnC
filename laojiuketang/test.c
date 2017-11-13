66f12874aba01b4233acf152a168317415e8360b
875280046 <33033796+875280046@users.noreply.github.com>
2017-11-12 22:49:04 -0500

    Add files via upload

---
+++ laojiuketang/test.c
@@ -0,0 +1,25 @@
+/*
+encryption array: array[i] + index + key
+in the same way dencryption array: newarray[i] - index - key
+
+*/
+#include<stdio.h>
+#include<string.h>
+#define KEY 3
+
+char* encryption(char*);
+
+int main(){
+	char array[] = {"hello"};
+	printf("%s\n", encryption(array));
+	return 0;
+}
+
+char* encryption(char* array){
+	int i = 0; 
+	int count = strlen(array);
+	for(i = 0; i < count; i++){
+		array[i] = array[i] + i + KEY;
+	} 
+	return array;
+}
